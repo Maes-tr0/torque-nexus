12fe01e31d9861c1be3216d90b5ce135a9dfb9cd
Maes-tr0 <nikitoc153@gmail.com>
2025-06-24 16:45:57 +0300

    Migrate from .env to Spring Boot profiles

--- src/main/java/ua/torque/nexus/TorqueNexusApplication.java
+++ src/main/java/ua/torque/nexus/TorqueNexusApplication.java
@@ -1,6 +1,5 @@
 package ua.torque.nexus;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,9 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TorqueNexusApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(TorqueNexusApplication.class, args);
     }
 }
