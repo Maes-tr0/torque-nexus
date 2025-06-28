a3164a3026df479cbb67792ccf9ffae6bd0bf8aa
Maes-tr0 <nikitoc153@gmail.com>
2025-06-28 15:16:02 +0300

    Fix class AuthMapper

--- src/main/java/ua/torque/nexus/auth/mapper/AuthMapper.java
+++ src/main/java/ua/torque/nexus/auth/mapper/AuthMapper.java
@@ -1,7 +1,6 @@
 package ua.torque.nexus.auth.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import ua.torque.nexus.auth.dto.request.RegistrationRequest;
 import ua.torque.nexus.user.model.User;
@@ -9,9 +8,6 @@ import ua.torque.nexus.user.model.User;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AuthMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "role", ignore = true)
-    @Mapping(target = "emailConfirmed", constant = "false")
     User toUser(RegistrationRequest request);
 }
 
